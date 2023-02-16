a53c3e2769c649c507baa7be5270cdf40cec6b95
Geoff Fox <fox.geoffrey@gmail.com>
2023-02-15 16:20:32 -0800

    Task: Demo: Develop the Express API

---
+++ server/api/routes/project.js
@@ -0,0 +1,6 @@
+module.exports = function (router) {
+    //GET: List of active projects
+    router.get('/project', function (req, res) {
+
+    });
+}
---
+++ server/api/routes/team.js
@@ -0,0 +1,6 @@
+module.exports = function (router) {
+    //GET: List of team Member
+    router.get('/team', function (req, res) {
+
+    });
+}
